c25f1839f6aaad1b54e1790a83e0080c52ade2c4
SalvaWebPr0 <salvawebpro@gmail.com>
2023-11-10 10:54:48 -0600

    change in Google tag manger handler

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,9 @@
 'use client';
-import BeforeAndAfter from '@/components/ctas/BeforeAndAfter';
 import ViewResultsComponent from '@/components/ctas/ViewResultsComponent';
 import HeroComponent from '@/components/hero/HeroComponent';
 import FaqComponent from '@/components/sections/FaqComponent';
-import TestimonialsComponent from '@/components/sections/TestimonialsComponent';
-import TestimonialsSlider from '@/components/sliders/TestimonialsSlider';
 import PageTransition from '@/components/transitions/PageTransition';
-import TagManager from 'react-gtm-module';
 import { ToastContainer } from 'react-toastify';
-
-const tagManagerArgs = {
-  gtmId: 'GTM-N4382WJN',
-};
-TagManager.initialize(tagManagerArgs);
 
 export default function Home() {
   return (
